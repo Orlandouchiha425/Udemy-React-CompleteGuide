6c750b41a073cee19bf353bc122079a64710a9ac
orlandouchiha425 <valadez425@gmail.com>
2023-03-02 04:13:47 -0500

    working with useStates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ExpenseItem from "./components/ExpenseItem.js";
 import Expenses from "./components/Expenses.js";
+import NewExpense from "./components/NewExpense/NewExpense.js";
 function App() {
   const expenses = [
     {
@@ -25,7 +26,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+     <NewExpense />
    <Expenses expenses={expenses}/>
    </div>
   );
---
+++ src/components/NewExpense/InvalidMessage.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react'
+
+function InvalidMessage() {
+    const [messageValidity, setMessageValidity] = useState('Invalid');
+    
+    function messageChangeHandler(event) {
+        const value = event.target.value;
+        if (value.trim().length < 3) {
+            setMessageValidity('Invalid')
+        } else {
+            setMessageValidity('Valid');
+        }
+    }
+    
+    return (
+        <form>
+            <label>Your message</label>
+            <input type="text" onChange={messageChangeHandler} />
+            <p>{messageValidity} message</p>
+        </form>
+    );
+}
+
+export default InvalidMessage
