604a58d1f9e088539890aa7960a9d12b4b92bf85
orlandouchiha425 <valadez425@gmail.com>
2023-03-07 04:02:26 -0500

    working udemy courses and useState practice /algorithms next

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,32 +1,57 @@
 import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css"
+import "./Expenses.css";
 import Card from "./Card";
-export default function Expenses({ expenses }) {
+import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
+import { useState } from "react";
+export default function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    console.log("Expenses.js");
+    console.log(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+
+        />
+        ))}
+        {/* <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
       />
       <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
       />
       <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
       />
-    </Card>
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      /> */}
+      </Card>
+    </div>
   );
 }
 
-
-
 //or you can do this
-
 
 // import ExpenseItem from "./ExpenseItem";
 // import "./Expenses.css"
